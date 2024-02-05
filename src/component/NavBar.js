363a0e3e0c8fe11fbcbe9399d46f6d5bf976751c
malik <malikmphomalope@gmail.com>
2024-02-05 16:49:45 +0200

    Creating Hover for the nav tabs and logo/brand name

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,8 +4,8 @@ import { Navbar, Nav } from 'react-bootstrap';
 function MyNavbar() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React Navbar</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Brand href="#home">Malik Malope</Navbar.Brand>
+      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
