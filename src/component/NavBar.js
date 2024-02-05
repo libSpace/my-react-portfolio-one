9f4fca24a610025129ed6ca861e48304fd2999e1
Malik Malope <140521344+libSpace@users.noreply.github.com>
2024-02-05 15:06:32 +0200

    Add files via upload

---
+++ src/component/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+function MyNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React Navbar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#education">Education</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contacts">Contacts</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
