c977e3aa7712ab73ffc736e339fac0fa92ad0bc7
Leroyson Malope <140521344+libSpace@users.noreply.github.com>
2024-02-06 09:58:33 +0000

    A horizontal line with a bottom shadow

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './NavBar.css';
-
+import React from "react";
+import "./NavBar.css";
 
 /* Icon libraries
 #################*/
 
-// Home icon 
+// Home icon
 import { IoHomeOutline } from "react-icons/io5";
 // About icon
 import { VscAccount } from "react-icons/vsc";
-// education and experience icon 
+// education and experience icon
 import { FiBookOpen } from "react-icons/fi";
 // projects
 import { GrProjects } from "react-icons/gr";
@@ -19,29 +18,31 @@ import { MdOutlineAttachEmail } from "react-icons/md";
 function NavBar() {
   return (
     <>
-    <nav className='nav-wrapper'>
-      <div className='nav-content'>
-        {/* <img className='logo' src='./src/images/logoname.png' alt='my-logo'/> */}
-        <div className='myLogo' >Malik Malope</div>
-        <ul>
-          <li className='menu-item'>
-          <IoHomeOutline className='my-icon'/>
-          </li>
-          <li className='menu-item'>
-          <VscAccount className='my-icon' />
-          </li>
-          <li className='menu-item'>
-            <FiBookOpen className='my-icon' />
-          </li>
-          <li className='menu-item'>
-            <GrProjects className='my-icon' />
-          </li>
-          <li className='menu-item'>
-            <MdOutlineAttachEmail className='my-icon' />
-          </li>
-        </ul>
-      </div>
-    </nav>
+      <nav className="nav-wrapper">
+        <div className="nav-content">
+          {/* <img className='logo' src='./src/images/logoname.png' alt='my-logo'/> */}
+          <div className="myLogo">Malik Malope</div>
+          <ul>
+            <li className="menu-item">
+              <IoHomeOutline className="my-icon" />
+            </li>
+            <li className="menu-item">
+              <VscAccount className="my-icon" />
+            </li>
+            <li className="menu-item">
+              <FiBookOpen className="my-icon" />
+            </li>
+            <li className="menu-item">
+              <GrProjects className="my-icon" />
+            </li>
+            <li className="menu-item">
+              <MdOutlineAttachEmail className="my-icon" />
+            </li>
+          </ul>
+        </div>
+
+        <hr class="nav-liner" />
+      </nav>
     </>
   );
 }
