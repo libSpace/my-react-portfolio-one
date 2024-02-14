33f0d4971e502bc1f46277eb9ae20ef690f987f1
libSpace <malikmphomalope@gmail.com>
2024-02-14 17:47:15 +0200

    Just to make sure

--- src/component/About.js
+++ src/component/About.js
@@ -5,44 +5,46 @@ import './About.css'
 
 export default function About() {
   return (
-    <div className='about-container'>
-      <div className='about'>
-        <div className='abt-head'>
-            <h1>Who am I</h1>
-            <hr className='abt-line'></hr>
-        </div>
+    <div clasName="thee">
+      <div className='about-container'>
+        <div className='about'>
+          <div className='abt-head'>
+              <h1>Who am I</h1>
+              <hr className='abt-line'></hr>
+          </div>
 
-        <div className='abt-content'>
-            <div className='pic-left'>
-                {/* my self picture */}
-                <img className='abt-img' src={img} alt='my pic' width={'350rem'}  />
-                <div class="fade-bottom"></div>
-            </div>
-            <div className='text-right'>
-              <p className='text'>
-                Hello, curious minds! 
-              </p>
-              <p className='text'>
-              I'm Malik Malope,
-              </p>
-              <p className='text'>
-              a perpetual learner on a quest for personal and professional growth. Beyond the resume, my story is shaped by a love for challenges, a commitment to adaptability, and an unwavering belief in the transformative power of continuous learning.  
-              Let's explore the boundless possibilities together!
-          
-              </p>            
-            </div>
-        </div>
-        <div className='box'>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className='abt-content'>
+              <div className='pic-left'>
+                  {/* my self picture */}
+                  <img className='abt-img' src={img} alt='my pic' width={'350rem'}  />
+                  <div class="fade-bottom"></div>
+              </div>
+              <div className='text-right'>
+                <p className='text'>
+                  Hello, curious minds! 
+                </p>
+                <p className='text'>
+                I'm Malik Malope,
+                </p>
+                <p className='text'>
+                a perpetual learner on a quest for personal and professional growth. Beyond the resume, my story is shaped by a love for challenges, a commitment to adaptability, and an unwavering belief in the transformative power of continuous learning.  
+                Let's explore the boundless possibilities together!
+            
+                </p>            
+              </div>
+          </div>
+          <div className='box'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
