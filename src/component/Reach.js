8d26d7499dad9c074f1764b920523058c9ccf4b4
libSpace <malikmphomalope@gmail.com>
2024-02-20 15:46:26 +0200

    Contact form validation done

--- src/component/Reach.js
+++ src/component/Reach.js
@@ -1,89 +1,77 @@
 import React from 'react';
+import Validation from './Validation';
 import './Reach.css';
 import reachSvg from './../images/phone-call.svg';
 import ReCAPTCHA from "react-google-recaptcha";
 // Icons from react icons
 import { FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { useState } from 'react';
+
+
+
 
 export default function Reach() {
-  const form = document.querySelector("form");
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    message:''
+  })
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault(); // Fix: add parentheses
-    // do nothing if form not validated
-    if (!validateForm(form)) return;
-  });
+  const [errors,setErrors] = useState({})
 
-  const validateForm = (form) => {
-    let valid = true;
-    // check for empty fields
-    let name = form.querySelector(".name");
-    let message = form.querySelector(".message");
-    let email = form.querySelector(".email");
-
-    if (name.value.trim() === "") { // Fix: check for empty string using trim()
-      giveError(name, "please enter your name");
-      valid = false;
-    }
-    if (message.value.trim() === "") { // Fix: check for empty string using trim()
-      giveError(message, "please enter a message");
-      valid = false;
-    }
-    // email validation
-    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    let emailValue = email.value;
-    if (!emailRegex.test(emailValue)) {
-      giveError(email, "Please enter a valid email");
-      valid = false;
-    }
-
-    return valid;
-  };
-
-  const giveError = (field, message) => {
-    let parentElement = field.parentElement;
-    parentElement.classList.add("error");
-    // if error msg already exists, remove it
-    let existingError = parentElement.querySelector(".err-msg");
-    if (existingError) {
-      existingError.remove();
-    }
-
-    let error = document.createElement("span");
-    error.textContent = message;
-    error.classList.add("err-msg");
-    parentElement.appendChild(error);
-  };
+  function handleInput(event){
+    const newObj = {...values, [event.target.name]: event.target.value}
+    setValues(newObj)
+  }
+  function handleValidation(event){
+    event.preventDefault();
+    setErrors(Validation(values));
+  }
 
   return (
     <>
       <div className='reach-container'>
         <div className='reach-left'>
           <h3 className='reach-heading'>Get in touch</h3>
-          <p className='reach-text'>Contact me for any project or any opportunity you have for me</p>
+          <p className='reach-text'>Contact me for any project or anyy oportunity you have for me</p>
 
-          <form action='#'>
+          <form action='#contact' onSubmit={handleValidation}>
             <div className='inputBox'>
-              <input 
+              <input  
+                style={{
+                  borderBottom: errors.name ? "3px solid red" : "3px solid green"
+                }} 
                 type='text' 
                 name='name' 
                 className='name' 
-                placeholder='Enter your name'/>
+                placeholder='Enter your name'
+                onChange={handleInput}/>
+                {errors.name && <p style={{color:"red"}}>{errors.name}</p>}
             </div>
             <div className='inputBox'>
               <input 
+                style={{
+                  borderBottom: errors.email ? "3px solid red" : "3px solid green"
+                }} 
                 type='text' 
                 name='email' 
                 className='email' 
-                placeholder='Enter your email'/>
+                placeholder='Enter your email'
+                onChange={handleInput}/>
+                {errors.email && <p style={{color:"red"}}>{errors.email}</p>}
             </div>
             <div className='inputBox'>
               <textarea 
+                style={{
+                  borderBottom: errors.message ? "3px solid red" : "3px solid green"
+                }} 
                 type='text' 
                 name='message' 
-                className='message' // Fix: Correct className
-                placeholder='Enter your message...'/>
+                className='message' 
+                
+                onChange={handleInput}/>
+                {errors.message && <p style={{color:"red"}}>{errors.message}</p>}
             </div>
             {/* <button class="g-recaptcha"
               data-sitekey="6LfCCngpAAAAAIY6K5cKTjHj-JNE3GmYbQrD0Ewo"
@@ -106,7 +94,7 @@ export default function Reach() {
             <FaLinkedin     className="reach-soc-icons" />
           </div>
         </div>
-        <span></span>
+   <span></span>
    <span></span>
    <span></span>
    <span></span>
@@ -143,5 +131,5 @@ export default function Reach() {
    <span></span>
       </div>
     </>
-  );
+  )
 }
---
+++ src/component/Validation.js
@@ -0,0 +1,22 @@
+import './Reach.css';
+export default function Validation(values) {
+    const errors = {};
+
+    const email_pattern = /^[^\s@]+@[^\s@]+.[^\s@]{2,6}$/;
+    
+    if(values.name ===""){
+        errors.name = "Name is Required";
+    }
+
+    if(values.email ===""){
+        errors.email ="Email is required";
+    }else if(!email_pattern.test(values.email)){
+        errors.email = "Email did not match";
+    }
+
+    if(values.message ===""){
+        errors.message = "Message is Required";
+    }
+
+    return errors;   
+}
